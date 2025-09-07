ec51969572a1c0dcbbeb13486733b42b40766eec
Towbee05 <olatiseoluwatobiloba@gmail.com>
2025-09-08 00:38:33 +0100

    Added responsiveness

--- client/todo-app/src/app/page.tsx
+++ client/todo-app/src/app/page.tsx
@@ -64,85 +64,88 @@ export default function Dashboard (){
     console.log(userList);
     return (
         <section className="min-h-screen w-full flex justify-center">
-            <div className="bg-[url(/img/bg-mobile-light.jpg)] w-full px-4 bg-no-repeat bg-contain dark:bg-[url(/img/bg-mobile-dark.jpg)] dark:bg-slate-950">
-                <div className="w-full flex justify-between items-center pt-22 pb-14 px-5">
-                    <h1 className="uppercase text-white font-black text-2xl tracking-[1.3rem] ">
-                        todo
-                    </h1>
-                    <button>
-                        <Image 
-                            src='/img/icon-sun.svg'
-                            alt='sun logo for light mode'
-                            width={30}
-                            height={30}
-                            className="block dark:hidden"
-                        />
-                        <Image 
-                            src='/img/icon-moon.svg'
-                            alt='moon logo for light mode'
-                            width={30}
-                            height={30}
-                            className="hidden dark:block"
-                        />
-                    </button>
-                </div>
-                <div className="space-y-10">
-                    <div className="flex justify-between items-center w-full bg-white dark:bg-slate-900 py-5 px-6 rounded-2xl">
-                        <div className="flex w-full gap-5 ">
-                            <input type="checkbox" name="completed" className="absolute opacity-0 w-0 h-0" id='checkmark' onChange={(e) => setIsChecked(e.target.checked)}/>
-                            <label htmlFor= "checkmark" className={`checkmark flex justify-center items-center w-8 h-8 border-2 border-gray-300 dark:border-2 dark:border-gray-600 dark:bg-slate-900 rounded-full ${ischecked? 'bg-linear-to-r from-purple-500 to-cyan-300': 'bg-white'}`}>
-                                <Image  
-                                    src='/img/icon-check.svg'
-                                    alt='checked icon'
-                                    width={10}
-                                    height={10}
-                                    className={ischecked? 'block': 'hidden'}
-                                /> 
-                            </label>
-                            <input type="text" placeholder="Create a new todo ..." className="dark:placeholder:text-gray-200 text-slate-600 text-lg font-bold focus:outline-0 dark:text-gray-400 border-b-2 border-b-slate-600 px-2 "/>
-                        </div>
-                        <button className="cursor-pointer text-slate-600 dark:text-gray-200 bg-purple-600 px-5 py-1 float-right rounded-xl">
-                            Add
+            <div className="bg-[url(/img/bg-mobile-light.jpg)] md:bg-[url(/img/bg-bg-desktop-light.jpg)] w-full px-4 bg-no-repeat bg-contain dark:bg-[url(/img/bg-mobile-dark.jpg)] md:dark:bg-[url(/img/bg-desktop-dark.jpg)] dark:bg-slate-950 flex justify-center">
+                <div className="max-w-[750px] w-full">
+                    <div className="w-full flex justify-between items-center pt-22 md:pt-32 pb-14 px-5">
+                        <h1 className="uppercase text-white font-black text-2xl tracking-[1.3rem] ">
+                            todo
+                        </h1>
+                        <button>
+                            <Image 
+                                src='/img/icon-sun.svg'
+                                alt='sun logo for light mode'
+                                width={30}
+                                height={30}
+                                className="block dark:hidden"
+                            />
+                            <Image 
+                                src='/img/icon-moon.svg'
+                                alt='moon logo for light mode'
+                                width={30}
+                                height={30}
+                                className="hidden dark:block"
+                            />
                         </button>
                     </div>
-                    <div>
-                        <ul className="flex flex-col bg-white dark:bg-slate-900 rounded-2xl shadow-3xl">
-                            {/* <TodoList /> */}
-                            {/* <hr className=""/> */}
-                            { userList.length === 0 ? (
-                                <p className="text-gray-300 text-center p-5">
-                                    No task added yet!!
-                                </p>
-                                ): (
-                                    userList.map((list) => {
-                                        return <TodoList key={list._id} editFlag deleteFlag setEditFlag={setEditFlag} setDeleteFlag={setDeleteFlag} list={list} setEditItem={setEditItem} setDeleteItem={setDeleteItem}/>
-                                    })
-                                )
-                            }
-                            <div className="flex justify-between items-center dark:text-gray-400 px-6 py-7">
-                                <p>
-                                    {(userList.filter(list => !list.completed)).length} items left
-                                </p>
-                                <button className="cursor-pointer">
-                                    Clear completed
-                                </button>
+                    <div className="space-y-10">
+                        <div className="flex justify-between items-center w-full bg-white dark:bg-slate-900 py-5 px-6 rounded-2xl">
+                            <div className="flex w-full gap-5 ">
+                                <input type="checkbox" name="completed" className="absolute opacity-0 w-0 h-0" id='checkmark' onChange={(e) => setIsChecked(e.target.checked)}/>
+                                <label htmlFor= "checkmark" className={`checkmark flex justify-center items-center w-8 h-8 border-2 border-gray-300 dark:border-2 dark:border-gray-600 dark:bg-slate-900 rounded-full ${ischecked? 'bg-linear-to-r from-purple-500 to-cyan-300': 'bg-white'}`}>
+                                    <Image  
+                                        src='/img/icon-check.svg'
+                                        alt='checked icon'
+                                        width={10}
+                                        height={10}
+                                        className={ischecked? 'block': 'hidden'}
+                                    /> 
+                                </label>
+                                <input type="text" placeholder="Create a new todo ..." className="dark:placeholder:text-gray-200 text-slate-600 text-lg font-bold focus:outline-0 dark:text-gray-400 border-b-2 border-b-slate-600 px-2 "/>
                             </div>
-                        </ul>
-                    </div>
-                    <div className="flex justify-center items-center w-full p-6 bg-white dark:bg-slate-900 rounded-2xl text-slate-600 dark:text-gray-400 gap-4 shadow-3xl">
-                        <button className='cursor-pointer active'>
-                            All
-                        </button>
-                        <button className="cursor-pointer">
-                            Active
-                        </button>
-                        <button className="cursor-pointer">
-                            Completed
-                        </button>
-                    </div>
-                    <div className="text-slate-600 dark:text-gray-400 text-center">
-                        <p>Drag and drop to reorder list</p>
-                    </div>
+                            <button className="cursor-pointer text-slate-600 dark:text-gray-200 bg-purple-600 px-5 py-1 float-right rounded-xl">
+                                Add
+                            </button>
+                        </div>
+                        <div>
+                            <ul className="flex flex-col bg-white dark:bg-slate-900 rounded-2xl shadow-3xl">
+                                {/* <TodoList /> */}
+                                {/* <hr className=""/> */}
+                                { userList.length === 0 ? (
+                                    <p className="text-gray-300 text-center p-5">
+                                        No task added yet!!
+                                    </p>
+                                    ): (
+                                        userList.map((list) => {
+                                            return <TodoList key={list._id} editFlag deleteFlag setEditFlag={setEditFlag} setDeleteFlag={setDeleteFlag} list={list} setEditItem={setEditItem} setDeleteItem={setDeleteItem}/>
+                                        })
+                                    )
+                                }
+                                <div className="flex justify-between items-center dark:text-gray-400 px-6 py-7">
+                                    <p>
+                                        {(userList.filter(list => !list.completed)).length} items left
+                                    </p>
+                                    <button className="cursor-pointer">
+                                        Clear completed
+                                    </button>
+                                </div>
+                            </ul>
+                        </div>
+                        <div className="flex justify-center items-center w-full p-6 bg-white dark:bg-slate-900 rounded-2xl text-slate-600 dark:text-gray-400 gap-4 shadow-3xl">
+                            <button className='cursor-pointer active'>
+                                All
+                            </button>
+                            <button className="cursor-pointer">
+                                Active
+                            </button>
+                            <button className="cursor-pointer">
+                                Completed
+                            </button>
+                        </div>
+                        <div className="text-slate-600 dark:text-gray-400 text-center">
+                            <p>Drag and drop to reorder list</p>
+                        </div>
+
+                </div>
 
                 </div>
           </div>
